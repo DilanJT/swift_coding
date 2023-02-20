4166b0f14c1441f31d2bed0e3d7a91da82c8dbaa
DilanJT <dicmandilan@gmail.com>
2023-02-20 00:24:49 -0800

    initialized a new project called hacker news

---
+++ IOS_by_anjela_yu/HackerNews/HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by user223941 on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
