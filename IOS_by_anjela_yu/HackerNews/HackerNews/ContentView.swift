58e6df9c4b996ba49eaac01018a01493b9a8cd2f
DilanJT <dicmandilan@gmail.com>
2023-02-20 17:03:57 +0800

    added UI to the hacker new app

--- IOS_by_anjela_yu/HackerNews/HackerNews/ContentView.swift
+++ IOS_by_anjela_yu/HackerNews/HackerNews/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            // For every single post in posts array Iam going to use Text component to pass the title propoerty of each post object
+            List(posts) { post in
+                Text(post.title)
+            }.navigationTitle("Hacker News")
+            
         }
-        .padding()
     }
 }
 
@@ -24,3 +24,20 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// Identifiable is a protocol that allow the List to record the order of the Post objects
+//struct Post: Identifiable{
+    // If we have a protocol called Identifiable we need to have an id inside the structure
+//    let id: String
+//    let title: String
+//}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola")
+]
+
+
+// https://hn.algolia.com/api
+// api -> https://hn.algolia.com/api/v1/search?tags=front_page
---
+++ IOS_by_anjela_yu/HackerNews/HackerNews/Models/PostData.swift
@@ -0,0 +1,25 @@
+//
+//  PostData.swift
+//  HackerNews
+//
+//  Created by user223941 on 2/20/23.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+    
+}
+
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String
+}
