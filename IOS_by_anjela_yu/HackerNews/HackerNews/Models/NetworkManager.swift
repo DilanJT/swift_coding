866bea56f78a959d8f00ef81b084ad42e1e67795
DilanJT <dicmandilan@gmail.com>
2023-02-23 19:28:06 +0800

    added the network manager class

---
+++ IOS_by_anjela_yu/HackerNews/HackerNews/Models/NetworkManager.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkManager.swift
+//  HackerNews
+//
+//  Created by user223941 on 2/23/23.
+//
+
+import Foundation
+
+// broadcast one or many object for any interested parties (var
+class NetworkManager: ObservableObject{
+    
+    func fetchData() {
+        if let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error == nil {
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        do {
+                            let results = try decoder.decode(Results.self, from: safeData)
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
