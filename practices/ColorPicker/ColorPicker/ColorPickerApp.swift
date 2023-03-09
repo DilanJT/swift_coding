8a74c34dce9b8fe264f8f40449899abaa914b065
DilanJT <dicmandilan@gmail.com>
2023-03-09 14:51:18 +0530

    created a new app called color picker with a single view and 2 components

---
+++ practices/ColorPicker/ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Dilan Jayamanne on 2023-03-09.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ practices/ColorPicker/ColorPicker/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  ColorPicker
+//
+//  Created by Dilan Jayamanne on 2023-03-09.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderView(label: "Label") .padding ()
+    }
+}
