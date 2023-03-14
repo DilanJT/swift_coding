01c01d479393232af4cbce2f7f114ea768a17659
DilanJT <dicmandilan@gmail.com>
2023-03-14 20:27:25 +0530

    finished the functionality of the distance calculator swift app

--- swift_iit/DistanceConverter/DistanceConverter/ContentView.swift
+++ swift_iit/DistanceConverter/DistanceConverter/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var textFieldValue: String = ""
-    @State private var unit_type = "unit"
-    @State private var converted_value = "value"
+    @State private var unit_type = "<<unit>>"
+    @State private var converted_value = "<<value>>"
     
     var buttonTexts = ["Ft", "cm", "Yd", "km"]
+    var buttonColors = [Color(.red), Color(.green), Color(.blue), Color(.purple)]
     
     var body: some View {
         VStack {
@@ -21,17 +22,27 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.bold)
             
+            Text("Enter the distance in meters")
+            
             TextField("Distance", text: $textFieldValue)
+                .padding()
+                .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0)))
+                .padding()
+            /*
+                .padding(.all, 10)
                 .frame(width: UIScreen.main.bounds.width * 0.9, height: 20)
-                .font(.headline)
-                .border(.black)
-                .multilineTextAlignment(.center)
+                .border(.black, width: 2)
+                .multilineTextAlignment(.center) */
                 
             HStack {
                 
-                ForEach((buttonTexts), id: \.self){ text in
+                ForEach((0...(buttonTexts.count-1)), id: \.self){ text in
                     ButtonView(
-                        buttonText: text
+                        buttonText: buttonTexts[text],
+                        unitType: $unit_type,
+                        converted_value: $converted_value,
+                        textFieldValue: textFieldValue,
+                        buttonColor: buttonColors[text]
                     )
                 }
             }
@@ -39,11 +50,17 @@ struct ContentView: View {
             Text("The distance in \(unit_type) is \(converted_value)")
             
             Image("flower")
+            Spacer()
             Button {
-                
+                // reset logic
+                // reset logic
+                self.unit_type = "<<unit>>"
+                self.converted_value = "<<value>>"
+                self.textFieldValue = ""
             }label: {
                 Text("Reset")
             }
+            .buttonStyle(.borderedProminent)
             
         }
         .padding()
@@ -59,24 +76,41 @@ struct ContentView_Previews: PreviewProvider {
 struct ButtonView: View {
     
     var buttonText: String
+    @Binding var unitType: String
+    @Binding var converted_value: String
+    var textFieldValue: String
+    var buttonColor: Color
     
     var body: some View {
         Button {
             
             if(buttonText == "Ft"){
                 // Ft logic
+                convert(textFieldValue: textFieldValue, unit_type: "Ft", change: 3.28)
+                
             }else if(buttonText == "cm"){
                 // cm logic
+                convert(textFieldValue: textFieldValue, unit_type: "cm", change: 100)
             }else if(buttonText == "Yd"){
                 // yd logic
+                convert(textFieldValue: textFieldValue, unit_type: "Yd", change: 1.09)
             }else if(buttonText == "km"){
                 // km logic
-            }else if(buttonText == "reset"){
-                // reset logic
+                convert(textFieldValue: textFieldValue, unit_type: "km", change: 0.001)
             }
             
         } label: {
             Text("\(buttonText)")
         }
+        .buttonStyle(.borderedProminent)
+        .tint(buttonColor)
+    }
+    
+    func convert(textFieldValue: String, unit_type: String, change: Double) {
+        self.unitType = unit_type
+        guard let meters = Double(textFieldValue) else {return}
+        let value = meters * change
+        
+        self.converted_value = String(format: "%.2f",value)
     }
 }
